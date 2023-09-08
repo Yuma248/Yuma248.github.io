4ee503f4c8a8f146f2f3cd7ca5516d4f860cdaf0
Yuma More <yuma248@hotmail.com>
2023-09-08 16:00:40 -0600

    deploy

---
+++ precache-manifest.701950a66ab9503a140a8500d62e5fa4.js
@@ -0,0 +1,362 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2da2fb41944ecef74e8a4a496a98f397",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5bee3d2c61f0fbf4d1df",
+    "url": "/static/css/main.7f6e0731.chunk.css"
+  },
+  {
+    "revision": "6a82976bda606796cb50",
+    "url": "/static/js/2.235e5505.chunk.js"
+  },
+  {
+    "revision": "d5d98c5782971ea94dfa10db66692a16",
+    "url": "/static/js/2.235e5505.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5bee3d2c61f0fbf4d1df",
+    "url": "/static/js/main.2ca47ffa.chunk.js"
+  },
+  {
+    "revision": "6895f4b772d7db031df5",
+    "url": "/static/js/runtime-main.726b3402.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "57ca3f622b67458355a133404630d321",
+    "url": "/static/media/CICESE.57ca3f62.svg"
+  },
+  {
+    "revision": "6350db52fa9a8bcb9406fc04b302d50e",
+    "url": "/static/media/ElasmoUABCS.6350db52.png"
+  },
+  {
+    "revision": "0d158f4d84a610a12657dd96c02edc62",
+    "url": "/static/media/FindersUni.0d158f4d.svg"
+  },
+  {
+    "revision": "68ff758ef7ca37db7d8caa6b497d93e0",
+    "url": "/static/media/FindersUni.68ff758e.png"
+  },
+  {
+    "revision": "c053ebe5114949270a4ec6cfc064d0eb",
+    "url": "/static/media/FindersUni2.c053ebe5.png"
+  },
+  {
+    "revision": "02ab62cbe3f06bd149ba6c0f07eff029",
+    "url": "/static/media/Genomics.02ab62cb.png"
+  },
+  {
+    "revision": "eec58604ae18e050756ba26a6ed6d3e1",
+    "url": "/static/media/Genomics.eec58604.svg"
+  },
+  {
+    "revision": "4457817ac2b9993c65e81aa05828fe9c",
+    "url": "/static/media/GoogleSans-Bold.4457817a.ttf"
+  },
+  {
+    "revision": "90773b6158663ab0fe78b32680733677",
+    "url": "/static/media/GoogleSans-BoldItalic.90773b61.ttf"
+  },
+  {
+    "revision": "0ecddcdeccb7761ce899aa9ad9f0ac3f",
+    "url": "/static/media/GoogleSans-Italic.0ecddcde.ttf"
+  },
+  {
+    "revision": "8d57e4014b18edef070d285746485115",
+    "url": "/static/media/GoogleSans-Medium.8d57e401.ttf"
+  },
+  {
+    "revision": "8fd3737925e83c87d78a13700ccf9a62",
+    "url": "/static/media/GoogleSans-MediumItalic.8fd37379.ttf"
+  },
+  {
+    "revision": "b5c77a6aed75cdad9489effd0d5ea411",
+    "url": "/static/media/GoogleSans-Regular.b5c77a6a.ttf"
+  },
+  {
+    "revision": "ac9aceda4f2e2ef0ff27505ce8ce5ebd",
+    "url": "/static/media/MELFU2.ac9aceda.png"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "75a997b2da32e43436a8519ad7d571a2",
+    "url": "/static/media/Peer_review.75a997b2.svg"
+  },
+  {
+    "revision": "85912d2fd567dbd0b7866187d58f68fd",
+    "url": "/static/media/Peer_review.85912d2f.png"
+  },
+  {
+    "revision": "f3fc01083bd042573854cc457760aed7",
+    "url": "/static/media/The-Aztec-world-creation-story-and-the-Cipactli-shark-monster-as-depicted-in-the-Codex.f3fc0108.png"
+  },
+  {
+    "revision": "bc4f2f2990dfa01763fe94a43d3de9a9",
+    "url": "/static/media/UABC.bc4f2f29.png"
+  },
+  {
+    "revision": "ee253b5e37e68fb3554f96c789b02c44",
+    "url": "/static/media/Yuma copy.ee253b5e.png"
+  },
+  {
+    "revision": "ee253b5e37e68fb3554f96c789b02c44",
+    "url": "/static/media/Yuma.ee253b5e.png"
+  },
+  {
+    "revision": "75d022616da374e79b9d4e67bc044b52",
+    "url": "/static/media/YumaCV.75d02261.pdf"
+  },
+  {
+    "revision": "d29cb5a1fee60c7c53f5a2d7713ebb8a",
+    "url": "/static/media/YumaCVOld.d29cb5a1.pdf"
+  },
+  {
+    "revision": "f2b88b516424905fcb383f2106ecb595",
+    "url": "/static/media/YumaDNA.f2b88b51.svg"
+  },
+  {
+    "revision": "b2ad24cd0f79e8fe405050ede6f94d0c",
+    "url": "/static/media/YumaData.b2ad24cd.svg"
+  },
+  {
+    "revision": "ad93062d3885d27a02a990fe9abd5104",
+    "url": "/static/media/YumaDeegrei.ad93062d.svg"
+  },
+  {
+    "revision": "6b2f41e27d9ce37811fe5d67ead04441",
+    "url": "/static/media/YumaDesk.6b2f41e2.svg"
+  },
+  {
+    "revision": "6e6b920bd5e6602a89c5df2df0b72173",
+    "url": "/static/media/YumaExperience.6e6b920b.svg"
+  },
+  {
+    "revision": "d1cde73044f4a383155f09b1133300b8",
+    "url": "/static/media/YumaFish.d1cde730.svg"
+  },
+  {
+    "revision": "65c09419acd5917d201415c561c38196",
+    "url": "/static/media/YumaGenomics.65c09419.svg"
+  },
+  {
+    "revision": "35aa6e8676a02761acb4b2c4a16b84d0",
+    "url": "/static/media/YumaIcon.35aa6e86.png"
+  },
+  {
+    "revision": "c84a865432926bac146f50048b5cf3eb",
+    "url": "/static/media/YumaIcon.c84a8654.svg"
+  },
+  {
+    "revision": "88c4d0e91c9d4da0ddcc1368ba721bb7",
+    "url": "/static/media/YumaMaill.88c4d0e9.svg"
+  },
+  {
+    "revision": "42d834ec744c500b5d06b0d38d5f6c29",
+    "url": "/static/media/YumaMain.42d834ec.svg"
+  },
+  {
+    "revision": "a80b24522f74607ced6ae2083dbb0085",
+    "url": "/static/media/Yumadata_science.a80b2452.svg"
+  },
+  {
+    "revision": "f352e884be1f7e399322e4ca4a3c0757",
+    "url": "/static/media/address_image.f352e884.svg"
+  },
+  {
+    "revision": "e5c4ba08c1a82ef1dc0c72885b61ca05",
+    "url": "/static/media/android.e5c4ba08.svg"
+  },
+  {
+    "revision": "52e0714e5ea7e3b96f50dbfb62de21d1",
+    "url": "/static/media/animated_logo.52e0714e.svg"
+  },
+  {
+    "revision": "b6b12c7d07e0569e771c8aedf81a7767",
+    "url": "/static/media/blogs_image.b6b12c7d.svg"
+  },
+  {
+    "revision": "dfcb7a585547c0b125d208b980d6f57f",
+    "url": "/static/media/cloud_infrastructure.dfcb7a58.svg"
+  },
+  {
+    "revision": "d6d4086fb1cade58b3f786620e17c3ef",
+    "url": "/static/media/contactMail.d6d4086f.png"
+  },
+  {
+    "revision": "6065cf898de84a5bbede1c170fc2bfc1",
+    "url": "/static/media/data_science.6065cf89.svg"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "caed892b2a0b817a8e275ae29638edaf",
+    "url": "/static/media/education.caed892b.svg"
+  },
+  {
+    "revision": "85d6957f6a0d678d1ac3932e5be4fcb2",
+    "url": "/static/media/experience.85d6957f.svg"
+  },
+  {
+    "revision": "06147b6cd88c7346cecd1edd060cd5de",
+    "url": "/static/media/fa-brands-400.06147b6c.ttf"
+  },
+  {
+    "revision": "4d0810d3586115ab33220e5b32599a4f",
+    "url": "/static/media/fa-brands-400.4d0810d3.svg"
+  },
+  {
+    "revision": "5063b105c7646c8043d58c5289f02cca",
+    "url": "/static/media/fa-brands-400.5063b105.eot"
+  },
+  {
+    "revision": "c5e0f14f88a828261ba01558ce2bf26f",
+    "url": "/static/media/fa-brands-400.c5e0f14f.woff"
+  },
+  {
+    "revision": "cccc9d29470e879e40eb70249d9a2705",
+    "url": "/static/media/fa-brands-400.cccc9d29.woff2"
+  },
+  {
+    "revision": "65b286af947c0d982ca01b40e1fcab38",
+    "url": "/static/media/fa-regular-400.65b286af.ttf"
+  },
+  {
+    "revision": "7b9568e6389b1f8ae0902cd39665fc1e",
+    "url": "/static/media/fa-regular-400.7b9568e6.svg"
+  },
+  {
+    "revision": "c1a866ec0e04a5e1915b41fcf261457c",
+    "url": "/static/media/fa-regular-400.c1a866ec.eot"
+  },
+  {
+    "revision": "c4f508e7c4f01a9eeba7f08155cde04e",
+    "url": "/static/media/fa-regular-400.c4f508e7.woff"
+  },
+  {
+    "revision": "f5f2566b93e89391da4db79462b8078b",
+    "url": "/static/media/fa-regular-400.f5f2566b.woff2"
+  },
+  {
+    "revision": "0bff33a5fd7ec390235476b4859747a0",
+    "url": "/static/media/fa-solid-900.0bff33a5.ttf"
+  },
+  {
+    "revision": "333bae208dc363746961b234ff6c2500",
+    "url": "/static/media/fa-solid-900.333bae20.woff"
+  },
+  {
+    "revision": "44d537ab79f921fde5a28b2c1636f397",
+    "url": "/static/media/fa-solid-900.44d537ab.woff2"
+  },
+  {
+    "revision": "8e4a6dcc692b3887f9f542cd6894d6d4",
+    "url": "/static/media/fa-solid-900.8e4a6dcc.eot"
+  },
+  {
+    "revision": "c2801fb415f03c7b170934769d7b5397",
+    "url": "/static/media/fa-solid-900.c2801fb4.svg"
+  },
+  {
+    "revision": "878ffc5778a689d470e4a85e77061a5c",
+    "url": "/static/media/feelingProud.878ffc57.svg"
+  },
+  {
+    "revision": "c4ade1e666d79ecec8bd8b0cc81b2859",
+    "url": "/static/media/frontiers.c4ade1e6.png"
+  },
+  {
+    "revision": "589bec7d632ecff9672dc4da97897bbd",
+    "url": "/static/media/fullstack.589bec7d.svg"
+  },
+  {
+    "revision": "ba18401b15f40a463898b706cc6ff08c",
+    "url": "/static/media/gcp_logo.ba18401b.png"
+  },
+  {
+    "revision": "e64de169a4222787bc49f3706282ef96",
+    "url": "/static/media/github_logo.e64de169.png"
+  },
+  {
+    "revision": "0c8a4c114ad00f7854e91550379b9b91",
+    "url": "/static/media/googleAssistant.0c8a4c11.jpg"
+  },
+  {
+    "revision": "c24ba79111a90758a3a472a906ab2901",
+    "url": "/static/media/googleAssistant.c24ba791.svg"
+  },
+  {
+    "revision": "29152cf756a1facead5393c32e373371",
+    "url": "/static/media/intel_logo.29152cf7.jpg"
+  },
+  {
+    "revision": "b05ae5b575a7e1125610944dc152c35f",
+    "url": "/static/media/jsFramework.b05ae5b5.svg"
+  },
+  {
+    "revision": "a6ce1e067cb40a22b5882e647a0327c0",
+    "url": "/static/media/macquarie.a6ce1e06.svg"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "dc75df84304aa6cbb377e7c37039bd41",
+    "url": "/static/media/moon.dc75df84.svg"
+  },
+  {
+    "revision": "c1a95f7335cbfb730b937e19e8830a53",
+    "url": "/static/media/programmer.c1a95f73.svg"
+  },
+  {
+    "revision": "487e76260e0b5352af475a598c63cb3f",
+    "url": "/static/media/projectsY_image.487e7626.svg"
+  },
+  {
+    "revision": "c403d6ac7c8d48bf3228bb5135c0a9c2",
+    "url": "/static/media/projects_image.c403d6ac.svg"
+  },
+  {
+    "revision": "bd5f4148a754968cb4444206539c565c",
+    "url": "/static/media/pwa.bd5f4148.png"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "3c786259cadd0813cb4717e084730100",
+    "url": "/static/media/skill.svg.2023_08_28_09_36_48.0.3c786259.svg"
+  },
+  {
+    "revision": "4db24c0e662ca0fbacc25b3c959afedc",
+    "url": "/static/media/sun.4db24c0e.svg"
+  },
+  {
+    "revision": "0288c5ec7a87ea73f8d14b12fc4bc94e",
+    "url": "/static/media/talksCardBack.0288c5ec.svg"
+  },
+  {
+    "revision": "b48e86c9a956d3a3a32ca4a5ae05ddc7",
+    "url": "/static/media/talksCardBackAlt.b48e86c9.svg"
+  },
+  {
+    "revision": "1e6c60ca52727cc88a7a9231db5e1d95",
+    "url": "/static/media/uabcs.1e6c60ca.svg"
+  },
+  {
+    "revision": "67a6a11c47edafb87cf24da66afe3dca",
+    "url": "/static/media/ui_ux_design.67a6a11c.svg"
+  }
+]);
